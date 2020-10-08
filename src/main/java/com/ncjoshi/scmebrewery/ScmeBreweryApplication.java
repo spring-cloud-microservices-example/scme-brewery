2957583c4cb59b78fe38526b320750b12e82c483
Nandan Joshi <nandan@gmx.de>
2020-10-08 23:17:35 +0200

    Initial commit of Spring Boot.

---
+++ src/main/java/com/ncjoshi/scmebrewery/ScmeBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.ncjoshi.scmebrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmeBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmeBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncjoshi/scmebrewery/ScmeBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncjoshi.scmebrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmeBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
